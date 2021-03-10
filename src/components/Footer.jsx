99c1ac7a640ffab330f8966ae3f0966afc4170fa
Hannaanaya <hannaanaya0511@gmail.com>
2021-03-10 16:34:15 -0600

    Platzi Conf

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/components/Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p className="Footer-title">Platzi Store</p>
+      <p className="Footer-copy">Visítanos en: <a href="https://platzi.com/home">Platzi.com</a> </p>
+    </div>
+  );
+}
+
+export default Footer;
